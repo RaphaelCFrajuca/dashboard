3236d408233c8684c7b75a2af238699b294d5cf2
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-04-24 15:04:39 -0300

    feat: adiciona interface para visualizar dados da API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react';
+import fetchUsersBySexualOrientation from '../api/fetchUserBySexualOrientation';
+
 const Home = () => {
-  return <div>home</div>;
+  const [usersByOrientation, setUsersByOrientation] = useState([]);
+
+  useEffect(() => {
+    const response = fetchUsersBySexualOrientation();
+    response.then((res) => setUsersByOrientation(res));
+  }, []);
+
+  return (
+    <>
+      <div>home</div>
+      {usersByOrientation?.map((el) => (
+        <div key={el.name}>
+          <p>{el.name}</p>
+          <p>{el.count}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Home;

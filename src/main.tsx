b075a70ea3dc99146349388fabfd1f51aeede51a
alevieiramoreira <alessandracvmoreira@gmail.com>
2023-05-22 17:45:45 -0300

    fix: adicionar tokens no contexto ao realizar o login

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './ContextProviders/AuthContext';
-import { QueryClientProvider } from 'react-query';
-import queryClient from './services/queryClient/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </QueryClientProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );

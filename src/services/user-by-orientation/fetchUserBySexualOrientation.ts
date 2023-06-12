197f920b072cec65c8e0be1cfdce8360efe5a323
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-06-12 15:00:32 -0300

    feat: criação de função get para buscar usuários por orientação

--- src/services/user-by-orientation/fetchUserBySexualOrientation.ts
+++ src/services/user-by-orientation/fetchUserBySexualOrientation.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 const URL = 'https://is-it-safe-api-v2.herokuapp.com/is-it-safe/dashboard/user';
-const jwtToken =
-  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJyb2xlcyI6IlJPTEVfQURNSU4iLCJpYXQiOjE2ODQxNzU2NjEsImV4cCI6MTY4NDE3NjU2MX0.LAjNkLJgkGbF9V8b-4HPO5Nv2ht-jUzvIST6VzMxRGBIF_g6S_TiK_dYiysMKhHyLeeLuqDz586CISQ_wJXUuw';
 type SexualOrientation = {
   name: string;
   count: number;
@@ -12,10 +10,12 @@ type DashBoardData = {
   sexual_orientation: SexualOrientation[];
 };
 
-export default async function fetchUsersBySexualOrientation() {
+export default async function fetchUsersBySexualOrientation(
+  token: string | null
+) {
   const response: DashBoardData = await axios
     .get(URL, {
-      headers: { Authorization: `Bearer ${jwtToken}` },
+      headers: { Authorization: `Bearer ${token}` },
     })
     .then((res) => res.data);
   return response.sexual_orientation;

cb0e6f53f12e0fbfff433673fa4e3aa939e5f86d
jaquelinepires <piresjaqueline17@email.com>
2023-07-21 00:25:47 -0300

    feat: Organize routes in separate file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { GlobalStyle } from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <GlobalStyle />
-    </BrowserRouter>
+    <App />
+    <GlobalStyle />
   </React.StrictMode>
 );

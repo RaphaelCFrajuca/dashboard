58e1f24e3beaccc9fd3adde155526d0aa16c3ea9
jaquelinepires <piresjaqueline17@email.com>
2023-08-02 19:24:06 -0300

    feat: Created users chart

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
 import Sidebar from '../../components/sidebar/Sidebar';
 import Header from '../../components/header/Header';
-import * as Style from './Home.styles';
 import { ChartLocationHome } from '../../components/Charts/ChartLocationHome/ChartLocationHome';
+import { ChartUserHome } from '../../components/Charts/ChartUserHome/ChartUserHome';
+import * as Style from './Home.styles';
 
 const Home = () => {
   return (
@@ -12,7 +13,11 @@ const Home = () => {
           <Header />
 
           <Style.Content>
-            <ChartLocationHome />
+            <ChartUserHome />
+            <Style.Whapper>
+              <ChartLocationHome />
+              {/* Adicionar o grafico de review aqui, com width: 730px; */}
+            </Style.Whapper>
           </Style.Content>
         </Style.HeaderContentContainer>
       </Style.PageContainer>
